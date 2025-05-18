bbd5fab5dcfa62b5ab6bbd20e82b591f51292663
GonzaloBouso <gonzalobouso002@gmail.com>
2025-05-18 19:10:02 -0300

    Fix Header

--- frontend/ResQfood-Front/src/components/layout/Header.jsx
+++ frontend/ResQfood-Front/src/components/layout/Header.jsx
@@ -1,4 +1,3 @@
-// src/components/layout/Header.jsx
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
