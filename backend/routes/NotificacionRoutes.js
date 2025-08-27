8884c4538f22620edefaf5861e3bf3212b1fd62d
GonzaloBouso <gonzalobouso002@gmail.com>
2025-08-26 21:10:10 -0300

    Fix NotificacionRoutes

--- backend/routes/NotificacionRoutes.js
+++ backend/routes/NotificacionRoutes.js
@@ -1,4 +1,3 @@
-// backend/routes/NotificacionRoutes.js
 import express from 'express';
 import { NotificacionController } from '../controllers/NotificacionController.js';
 import { requireAuth } from '../middlewares/autMiddleware.js';
@@ -6,7 +5,7 @@ import { requireAuth } from '../middlewares/autMiddleware.js';
 const router = express.Router();
 
 
-router.get('/mis-notificaciones', requireAuth, NotificacionController.getMisNotificaciones);
+router.get('/', requireAuth, NotificacionController.getMisNotificaciones);
 
 router.patch('/marcar-como-leidas', requireAuth, NotificacionController.marcarNotificacionesComoLeidas);
 
