b9068fda8d6e088ff2cc175311eaa4471d388c87
GonzaloBouso <gonzalobouso002@gmail.com>
2025-08-29 23:01:24 -0300

    add console.log solicitudController

--- backend/controllers/SolicitudController.js
+++ backend/controllers/SolicitudController.js
@@ -314,6 +314,11 @@ export class SolicitudController {
             res.status(200).json({ message: 'Solicitud cancelada exitosamente.', solicitud });
         } catch (error) {
             console.error('Error al cancelar la solicitud:', error);
+            console.error("--- ERROR FATAL en aceptarSolicitudYProponerHorario ---");
+            console.error("Mensaje:", error.message);
+            console.error("Stack Trace:", error.stack);
+            console.error("----------------------------------------------------");
+            res.status(500).json({ message: `Error interno al aceptar la solicitud: ${error.message}` });
             res.status(500).json({ message: "Error interno del servidor.", errorDetails: error.message });
         }
     }
